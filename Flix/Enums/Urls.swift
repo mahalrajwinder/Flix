583e8c52ba27aed523dc7d260ff35deda45709e9
Rajwinder Singh <44937101+mahalrajwinder@users.noreply.github.com>
2019-09-12 11:34:32 -0700

    Add Url enum with base urls for API calls.

---
+++ Flix/Enums/Urls.swift
@@ -0,0 +1,14 @@
+//
+//  Urls.swift
+//  Flix
+//
+//  Copyright © 2019 Rajwinder Singh. All rights reserved.
+//
+
+import Foundation
+
+enum Url: String
+{
+    case baseUrl = "https://api.themoviedb.org/3/movie/"
+    case APIKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+}
