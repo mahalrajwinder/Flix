18d7f02b674b0478da37c3959e1a4c0de26677a1
Rajwinder Singh <44937101+mahalrajwinder@users.noreply.github.com>
2019-09-12 11:35:28 -0700

    Add Movie Model for storing movie data.

---
+++ Flix/Models/Movie.swift
@@ -0,0 +1,20 @@
+//
+//  Movie.swift
+//  Flix
+//
+//  Copyright © 2019 Rajwinder Singh. All rights reserved.
+//
+
+import Foundation
+
+
+struct Movie
+{
+    let title: String
+    let overview: String
+    let releaseDate: String
+    let posterPath: Any
+    let backdropPath: Any
+    let avgVotes: Double
+    let id: Int
+}
