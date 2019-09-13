3c1d0d1299e0076b34ddfe8669768e1a8ed7212c
Rajwinder Singh <44937101+mahalrajwinder@users.noreply.github.com>
2019-09-13 10:24:01 -0700

    Add case based functions as well as some static functions to generate requested URL.

--- Flix/Enums/Urls.swift
+++ Flix/Enums/Urls.swift
@@ -7,10 +7,51 @@
 
 import Foundation
 
+
 enum Url: String
 {
-    case baseUrl = "https://api.themoviedb.org/3/movie/"
+    case baseURL = "https://api.themoviedb.org/3/"
     case basePosterUrl = "https://image.tmdb.org/t/p/w185"
     case baseBackdropUrl = "https://image.tmdb.org/t/p/w780"
-    case APIKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+    case APIKey = "?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
+    
+    case upcoming, popular
+    case nowPlaying = "now_playing"
+    case topRated = "top_rated"
+    case dailyTrending = "trending/movie/day"
+    
+    
+    func url(numPage: Int) -> URL
+    {
+        let urlPath = Url.APIKey.rawValue + "&page=" + String(numPage)
+        
+        if self.rawValue == Url.dailyTrending.rawValue
+        {
+            return URL(string: Url.baseURL.rawValue + self.rawValue + urlPath)!
+        }
+        else
+        {
+            return URL(string: Url.baseURL.rawValue + "movie/" + self.rawValue + urlPath)!
+        }
+    }
+    
+    
+    static func latest() -> URL
+    {
+        return URL(string: Url.baseURL.rawValue + "movie/latest" + Url.APIKey.rawValue)!
+    }
+    
+    
+    static func video(movieID: Int) -> URL
+    {
+        return URL(string: Url.baseURL.rawValue + "movie/" + String(movieID) + "/videos" + Url.APIKey.rawValue)!
+    }
+    
+    
+    static func similar(numPage: Int, movieID: Int) -> URL
+    {
+        let urlPath = Url.APIKey.rawValue + "&page=" + String(numPage)
+        
+        return URL(string: Url.baseURL.rawValue + "movie/" + String(movieID) + "/similar" + urlPath)!
+    }
 }
