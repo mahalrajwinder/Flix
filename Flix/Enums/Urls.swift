ff4dfa515971475b4a070cf992aaf18718200054
Rajwinder Singh <44937101+mahalrajwinder@users.noreply.github.com>
2019-09-12 23:13:26 -0700

    Add base URL for poster and backdrop.

--- Flix/Enums/Urls.swift
+++ Flix/Enums/Urls.swift
@@ -10,5 +10,7 @@ import Foundation
 enum Url: String
 {
     case baseUrl = "https://api.themoviedb.org/3/movie/"
+    case basePosterUrl = "https://image.tmdb.org/t/p/w185"
+    case baseBackdropUrl = "https://image.tmdb.org/t/p/w780"
     case APIKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
 }
