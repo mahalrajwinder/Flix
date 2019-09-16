c0dfa3dc6e5630fad0a25198dab9b22ac9349269
Rajwinder Singh <44937101+mahalrajwinder@users.noreply.github.com>
2019-09-15 23:03:08 -0700

    Struct with layout sizes.

---
+++ Flix/Enums/Sizes.swift
@@ -0,0 +1,19 @@
+//
+//  Sizes.swift
+//  Flix
+//
+//  Copyright © 2019 Rajwinder Singh. All rights reserved.
+//
+
+import UIKit
+
+
+struct LayoutSizes
+{
+    let sidePadding: CGFloat
+    let interItemSpace: CGFloat
+    let cellWidth: CGFloat
+    let cellHeight: CGFloat
+    let headerHeight: CGFloat
+    let headerTopPadding: CGFloat
+}
